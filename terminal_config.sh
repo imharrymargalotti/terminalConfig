e5c58b457f019feb22676fcbe1b0c08507cffa9e
Harry <imharrymargalotti@gmail.com>
2022-09-05 20:38:31 -0500

    here's a bunch of command line utility I like

---
+++ terminal_config.sh
@@ -0,0 +1,72 @@
+brew install ranger #: cracked file browser
+brew install ncdu #: see what takes up all that space
+brew install node #: Platform built on V8 to build network applications
+brew install git #: Distributed revision control system
+brew install wget #: Internet file retriever
+brew install python3 #: Interpreted, interactive, object-oriented programming language
+brew install pkg-config #: Manage compile and link flags for libraries
+brew install chromedriver #: Tool for automated testing of webapps across many browsers
+brew install youtube-dl #: Download YouTube videos from the command-line
+brew install libyaml #: YAML Parser
+brew install autoconf #: Automatic configure script builder
+#brew install redis #: Persistent key-value database, with built-in net interface
+brew install pidof #: Display the PID number for a given process name
+brew install tree #: Display directories as trees (with optional color/HTML output)
+brew install jq #: Lightweight and flexible command-line JSON processor
+#brew install docker #: Pack, ship and run any application as a lightweight container
+brew install htop #: Improved top (interactive process viewer)
+brew install nvm #: Manage multiple Node.js versions
+brew install pyenv #: Python version management
+brew install graphviz #: Graph visualization software from AT&T and Bell Labs
+brew install unrar #: Extract, view, and test RAR archives
+brew install git-lfs #: Git extension for versioning large files
+brew install gdb #: GNU debugger
+brew install sqlite #: Command-line interface for SQLite
+brew install watch #: Executes a program periodically, showing output fullscreen
+brew install p7zip #: 7-Zip (high compression file archiver) implementation
+brew install flow #: Static type checker for JavaScript
+brew install httpie #: User-friendly cURL replacement (command-line HTTP client)
+#brew install docker-compose #: Isolated development environments using Docker
+brew install pandoc #: Swiss-army knife of markup format conversion
+brew install tig #: Text interface for Git repositories
+brew install ack #: Search tool like grep, but optimized for programmers
+#brew install docker-machine: Create Docker hosts locally and on cloud providers
+brew install pyenv-virtualenv #: Pyenv plugin to manage virtualenv
+brew install hub #: Add GitHub support to git on the command-line
+brew install thefuck #: Programatically correct mistyped console commands
+brew install highlight #: Convert source code to formatted text with syntax highlighting
+brew install ssh-copy-id #: Add a public key to a remote machine's authorized_keys file
+brew install certbot #: Tool to obtain certs from Let's Encrypt and autoenable HTTPS
+brew install dos2unix #: Convert text between DOS, UNIX, and Mac formats
+brew install rust #: Safe, concurrent, practical language
+brew install mtr #: 'traceroute' and 'ping' in a single tool
+brew install doxygen #: Generate documentation for several programming languages
+brew install pv #: Monitor data's progress through a pipe
+brew install autojump #: Shell extension to jump to frequently used directories
+brew install hadoop #: Framework for distributed processing of large data sets
+brew install lynx #: Text-based web browser
+brew install cloc #: Statistics utility to count lines of code
+brew install mas #: Mac App Store command-line interface
+brew install speedtest-cli #: Command-line interface for https://speedtest.net bandwidth tests
+brew install fswatch #: Monitor a directory for changes and run a shell command
+brew install httrack #: Website copier/offline browser
+brew install boot2docker #: Lightweight Linux for Docker
+brew install s3cmd #: Command-line tool for the Amazon S3 service
+brew install terminal-notifier #: Send macOS User Notifications from the command-line
+brew install fortune #: Infamous electronic fortune-cookie generator
+brew install pwgen #: Password generator
+brew install scipy #: Software for mathematics, science, and engineering
+brew install bash-git-prompt #: Informative, fancy bash prompt for Git users
+brew install pass #: Password manager
+brew install openconnect #: Open client for Cisco AnyConnect VPN
+brew install diff-so-fancy #: Good-lookin' diffs with diff-highlight and more
+brew install curl #: Get a file from an HTTP, HTTPS or FTP server
+brew install cmatrix #: Console Matrix
+brew install xpdf #: PDF viewer
+brew install fcrackzip #: Zip password cracker
+brew install terminator #: Multiple terminals in one window
+brew install reaver #: Implements brute force attack to recover WPA/WPA2 passkeys
+brew install m-cli #: Swiss Army Knife for macOS
+brew install git-credential-manager #: Stores Git credentials for Visual Studio Team Services
+brew install qrencode #: QR Code generation
+brew install gibo #: Access GitHub's .gitignore boilerplates
